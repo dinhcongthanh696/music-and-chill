a7490e4e649d0782d5b60d31929693dcc8dbc54a
NguyenTheThai <thainthe150042@fpt.edu.vn>
2022-04-16 23:57:08 +0700

    upload code

--- src/Components/PlayMusic.js
+++ src/Components/PlayMusic.js
@@ -1,13 +1,37 @@
 import './PlayMusic.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart, faEllipsisH} from "@fortawesome/free-solid-svg-icons";
 
 function PlayMusic({
     isDisplayedPlayMusic
 }){
     return (
         <div className='play-music' style={{
-            display : isDisplayedPlayMusic ? 'block' : 'none'
+            opacity : isDisplayedPlayMusic ? 1 : 0
         }}>
-            <h1>Play Music</h1>
+            <div className='play-music__left'>
+                <div className='play-music__image'>
+                    <h1>Ảnh</h1>
+                </div>
+                <div className='play-music__description'>
+                    <h1 className='play-music__song'>Tên bài hát</h1>
+                    <h1 className='play-music__singer'>Tên ca sĩ</h1>
+                </div>
+                <div className='play-music__option'>
+                    <span>
+                    <FontAwesomeIcon icon={faHeart} className='play-music__icon'></FontAwesomeIcon>
+                    </span>
+                    <span>
+                    <FontAwesomeIcon icon={faEllipsisH} className='play-music__icon'></FontAwesomeIcon>
+                    </span>
+                </div>
+            </div>
+            <div className='play-music__center'>
+                <h1>Center</h1>
+            </div>  
+            <div className='play-music__right'>
+                <h1>Right</h1>
+            </div>
         </div>
     )
 }
