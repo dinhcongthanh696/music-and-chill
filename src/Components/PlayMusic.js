d1091e6b8937e6ebc5bb66fa09f5cf70794c7fed
Dinh Cong Thanh <thanhdche150982@fpt.edu.vn>
2022-04-18 13:57:06 +0700

    Music player update

--- src/Components/PlayMusic.js
+++ src/Components/PlayMusic.js
@@ -1,13 +1,67 @@
 import './PlayMusic.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart , faEllipsisH , faCirclePlay , faCirclePause} from "@fortawesome/free-solid-svg-icons";
-import {useState} from 'react'
+import {useState , useRef , useEffect} from 'react'
 import {MUSICPLAYERFOOTER} from './Constants.js'
 function PlayMusic({
     isDisplayedPlayMusic
 }){
     const [isLiked,setLiked] = useState(false);
     const [isPLaying,setPlaying] = useState(false);
+    const [currentAudioTime,setCurrentAudioTime] = useState(0);
+    const seekSliderRef = useRef();
+
+    const handlePlayMusic = () => {
+        setPlaying(!isPLaying);
+        if(isPLaying){
+            audioRef.current.pause();
+            clearInterval(musicPlayerInterval.current);
+        }else{
+            audioRef.current.play();
+            musicPlayerInterval.current = setInterval(() => {
+                setCurrentAudioTime(prevState => {
+                    return parseInt(prevState) + 1;
+                });
+                seekSliderRef.current.value++;
+                seekSliderRef.current.style.background = `linear-gradient(to right, #05d7f7 0% , #05d7f7 ${(seekSliderRef.current.value - seekSliderRef.current.min) / (seekSliderRef.current.max - seekSliderRef.current.min) * 100}% , #fff ${(seekSliderRef.current.value - seekSliderRef.current.min) / (seekSliderRef.current.max - seekSliderRef.current.min) * 100}% , #fff 100%)`;
+            },1000);
+        }
+    }
+
+
+    const toRealTime = (timeValue) => {
+        const minute = Math.floor(Math.floor(timeValue) / 60);
+        const second = (Math.floor(timeValue) - (minute * 60));
+        const result = minute + ":" + (second < 10 ? "0"+second : second);
+        return result;
+    }
+
+    const audioRef = useRef();
+    const musicPlayerInterval = useRef();
+
+    useEffect(() => {
+        const min = seekSliderRef.current.min;
+        const max = seekSliderRef.current.max;
+        const value = 0;
+        seekSliderRef.current.value = value;
+        const breakPoint = (value - min) / (max - min) * 100;
+        seekSliderRef.current.style.background = "linear-gradient(to right, #05d7f7 0% , #05d7f7 "+ breakPoint +"% , #fff "+ breakPoint + "% , #fff 100%)";
+        seekSliderRef.current.addEventListener("input",() => {
+            seekSliderRef.current.style.background = `linear-gradient(to right, #05d7f7 0% , #05d7f7 ${(seekSliderRef.current.value - seekSliderRef.current.min) / (seekSliderRef.current.max - seekSliderRef.current.min) * 100}% , #fff ${(seekSliderRef.current.value - seekSliderRef.current.min) / (seekSliderRef.current.max - seekSliderRef.current.min) * 100}% , #fff 100%)`;
+            setCurrentAudioTime(seekSliderRef.current.value);
+            audioRef.current.currentTime = seekSliderRef.current.value;
+        })
+    },[])
+
+    useEffect(() => {
+        if(currentAudioTime == MUSICPLAYERFOOTER.duration){
+            clearInterval(musicPlayerInterval.current);
+            seekSliderRef.current.value = 0;
+            seekSliderRef.current.style.background = `linear-gradient(to right, #05d7f7 0% , #05d7f7 ${(0 - seekSliderRef.current.min) / (seekSliderRef.current.max - seekSliderRef.current.min) * 100}% , #fff ${(0 - seekSliderRef.current.min) / (seekSliderRef.current.max - seekSliderRef.current.min) * 100}% , #fff 100%)`;
+            setCurrentAudioTime(0);
+            setPlaying(false);
+        }
+    },[currentAudioTime])
 
     return (
         <div className='play-music' style={{
@@ -31,7 +85,21 @@ function PlayMusic({
                 </div>
             </div>
             <div className='play-music__center'>
-                <FontAwesomeIcon icon={isPLaying ? faCirclePause : faCirclePlay} className='play-music__icon play-music__icon--size-s' onClick={() => setPlaying(!isPLaying)}></FontAwesomeIcon>
+                <audio ref={audioRef}>
+                    <source type='audio/mpeg' src='./images/tinh-da-day-mot-tim.mp3'></source>
+                </audio>
+                <div className='play-music__controls'>
+                    <FontAwesomeIcon 
+                    icon={isPLaying ? faCirclePause : faCirclePlay} 
+                    className='play-music__icon play-music__icon--size-s' 
+                    onClick={handlePlayMusic}>
+                    </FontAwesomeIcon>
+                </div>
+                <div className='play-music__seeker'>
+                    <span className='play-music__time'>{toRealTime(currentAudioTime)}</span>
+                    <input type='range' className='play-music__slider' min='0' max={MUSICPLAYERFOOTER.duration} ref={seekSliderRef}/>
+                    <span className='play-music__time'>{toRealTime(MUSICPLAYERFOOTER.duration - currentAudioTime)}</span>
+                </div>
             </div>  
             <div className='play-music__right'>
                 <h1>Right</h1>
